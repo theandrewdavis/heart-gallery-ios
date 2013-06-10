6919efb34a35f72d04e871a6bf974a4801f15c88
Andrew Davis <theandrewdavis@gmail.com>
2013-06-10 08:28:48 -0400

    Added new project for creating a table from JSON

---
+++ ToyJsonTable/ToyJsonTable/HGViewController.h
@@ -0,0 +1,13 @@
+//
+//  HGViewController.h
+//  ToyJsonTable
+//
+//  Created by Andrew Davis on 6/10/13.
+//  Copyright (c) 2013 Andrew Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGViewController : UIViewController
+
+@end
---
+++ ToyJsonTable/ToyJsonTable/HGViewController.m
@@ -0,0 +1,22 @@
+//
+//  HGViewController.m
+//  ToyJsonTable
+//
+//  Created by Andrew Davis on 6/10/13.
+//  Copyright (c) 2013 Andrew Davis. All rights reserved.
+//
+
+#import "HGViewController.h"
+
+@interface HGViewController ()
+
+@end
+
+@implementation HGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
