dbc97db874978fc1be6125996d4e07b95b79626a
Andrew Davis <theandrewdavis@gmail.com>
2013-07-01 08:28:46 -0400

    Added mysql setup to server

--- server/setup.sh
+++ server/setup.sh
@@ -9,13 +9,16 @@ ln -s /vagrant/web /var/www
 # Set up php
 apt-get install -y php5=5.3.10-1ubuntu3.6 php5-mysql
 
+# mysql connection info
+MYSQL_DB=hg
+MYSQL_USER=hguser
+MYSQL_USER_PASSWORD=i25iyOnACVax5y3NQAn
+MYSQL_ROOT_PASSWORD=CBdL7HhUJO0GOUpNIM1
+
 # Set up mysql
-# DEBIAN_FRONTEND=noninteractive apt-get install -y -q mysql-server
-# MYSQL_TABLE=hg
-# MYSQL_USER=hguser
-# MYSQL_USER_PASSWORD=i25iyOnACVax5y3NQAn
-# MYSQL_ROOT_PASSWORD=CBdL7HhUJO0GOUpNIM1
-# mysqladmin -u root create ${MYSQL_TABLE}
-# mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_TABLE}.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USERPASSWORD}'"
-# mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
+DEBIAN_FRONTEND=noninteractive apt-get install -y -q mysql-server
+mysqladmin -uroot create ${MYSQL_DB}
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}'"
+mysql -u${MYSQL_USER} -p${MYSQL_USER_PASSWORD} ${MYSQL_DB} < /vagrant/hg-database.sql
+mysqladmin -uroot password ${MYSQL_ROOT_PASSWORD}
 
