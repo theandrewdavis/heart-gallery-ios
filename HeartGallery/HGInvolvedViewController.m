dd7db62b441ce5dd52d3d7027e17a402b56fcd99
Andrew Davis <theandrewdavis@gmail.com>
2014-02-02 16:38:29 -0500

    Fixed amazon wish list link

--- HeartGallery/HGInvolvedViewController.m
+++ HeartGallery/HGInvolvedViewController.m
@@ -15,13 +15,12 @@
 
     self.navigationItem.title = @"Get Involved";
 
-    NSString *text = @"<style>li {margin-left: -20px;} ul {font-size: 18px;}</style><ul><li>Make a tax deductible donation by clicking <a href=\"http://www.heartgalleryalabama.com/support.php\">here</a></li><li>If you would like to bring the exhibit to your area please <a href=\"mailto:info@heartgalleryalabama.com\">contact us!</a></li><li>Grant a wish from our <a href=\"http://www.amazon.com/wishlist/226536JX5Q1PP/ref=cm_sw_r_fa_ws_Yp5Dob0MPX9QJ\">wish list</a> on Amazon</li><li>Share your talent - <a href=\"mailto:info@heartgalleryalabama.com\">volunteer</a></li><li>Donate stock by <a href=\"mailto:info@heartgalleryalabama.com\">clicking here</a> for more information</li><li><a href=\"http://www.heartgalleryalabama.com/support.php\">Purchase</a> a Heart Gallery Alabama t-shirt (put the number of shirts and sizes in the information line)<ul><li>Short sleeve: $15</li><li>Long sleeve: $20</li></ul></li><li><a href=\"http://www.heartgalleryalabama.com/support.php\">Show</a> your love for adoption with a Heart Gallery decal<ul><li>Suggested donation of $3 ea. Add $1 for shipping</li></ul></li></ul>";
+    NSString *text = @"<style>li {margin-left: -20px;} ul {font-size: 18px;}</style><ul><li>Make a tax deductible donation by clicking <a href=\"http://www.heartgalleryalabama.com/support.php\">here</a></li><li>If you would like to bring the exhibit to your area please <a href=\"mailto:info@heartgalleryalabama.com\">contact us!</a></li><li>Grant a wish from our <a href=\"http://www.amazon.com/wishlist/226536JX5Q1PP/\">wish list</a> on Amazon</li><li>Share your talent - <a href=\"mailto:info@heartgalleryalabama.com\">volunteer</a></li><li>Donate stock by <a href=\"mailto:info@heartgalleryalabama.com\">clicking here</a> for more information</li><li><a href=\"http://www.heartgalleryalabama.com/support.php\">Purchase</a> a Heart Gallery Alabama t-shirt (put the number of shirts and sizes in the information line)<ul><li>Short sleeve: $15</li><li>Long sleeve: $20</li></ul></li><li><a href=\"http://www.heartgalleryalabama.com/support.php\">Show</a> your love for adoption with a Heart Gallery decal<ul><li>Suggested donation of $3 ea. Add $1 for shipping</li></ul></li></ul>";
 
     UIWebView *textView = [[UIWebView alloc] initWithFrame:self.view.bounds];
     textView.delegate = self;
     [textView loadHTMLString:text baseURL:nil];
     [self.view addSubview:textView];
-    
 }
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
